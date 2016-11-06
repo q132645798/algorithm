7eba35d4fa3cd7fcc8ab972e7006dd9d7b65a52b
Lin <q132645798@gmail.com>
2016-11-07 00:03:18 +0800

    upate ex3

--- ex3.py
+++ ex3.py
@@ -2,16 +2,14 @@
 # encoding: utf8
 
 import time
-
+import random
+                
 def DnC(ary):
     if(len(ary) <= 3): return sorted(ary)
     d = (len(ary)+1) / 3
-    sal = DnC(ary[:d])
-    sam = DnC(ary[d:d*2])
-    sar = DnC(ary[d*2:])
-    tmp = []
+    sal, sam, sar, tmp = DnC(ary[:d]), DnC(ary[d:d*2]), DnC(ary[d*2:]), []
     while(not(len(sal) == 0 and len(sam) == 0 and len(sar) == 0)):
-        ta = [0]
+        ta = []
         if(len(sal) != 0 and len(sam) != 0 and len(sar) != 0):
             ta = sal if(sal[0] <= sam[0] and sal[0] <= sar[0]) \
                     else sam if(sam[0] <= sal[0] and sam[0] <= sar[0]) else sar
@@ -21,16 +19,22 @@ def DnC(ary):
             ta = sal if(sal[0] <= sar[0]) else sar
         elif(len(sar) == 0 and len(sal) != 0 and len(sam) != 0):
             ta = sal if(sal[0] <= sam[0]) else sam
-        tmp.append(ta[0])
-        ta.pop(0)
+        if(len(ta) != 0):
+            tmp.append(ta[0])
+            ta.pop(0)
         ta = sal if(len(sal) != 0 and len(sam) == 0 and len(sar) == 0) else \
                 sam if(len(sam) != 0 and len(sal) == 0 and len(sar) == 0) else \
                 sar if(len(sar) != 0 and len(sal) == 0 and len(sam) == 0) else []
-        for i in ta:
+        for i in sorted(ta):
             tmp.append(i)
             ta.pop(0)
     return tmp
 
-a = [120, 3, 1, 1321, 40, 2, 5 , 10, 1, 212, 23126, 312321, 5241, -123, -131]
-print('Array length is {}'.format(len(a)))
+a = []
+for i in range(1, 101):
+    a.append(random.randint(1, 10000))
+print('[*] Array is:')
+print(a)
+print('[*] Array length is {}'.format(len(a)))
+print('[*] Sorted')
 print DnC(a)
