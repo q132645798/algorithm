01d152ebc1a0b598d7f5c349ea1b62ef6e52c25f
Lin <q132645798@gmail.com>
2016-11-06 23:34:09 +0800

    update ex3

--- ex3.py
+++ ex3.py
@@ -10,10 +10,9 @@ def DnC(ary):
     sam = DnC(ary[d:d*2])
     sar = DnC(ary[d*2:])
     tmp = []
-    while True:
+    while(not(len(sal) == 0 and len(sam) == 0 and len(sar) == 0)):
         ta = [0]
-        if(len(sal) == 0 and len(sam) == 0 and len(sar) == 0): break       
-        elif(len(sal) != 0 and len(sam) != 0 and len(sar) != 0):
+        if(len(sal) != 0 and len(sam) != 0 and len(sar) != 0):
             ta = sal if(sal[0] <= sam[0] and sal[0] <= sar[0]) \
                     else sam if(sam[0] <= sal[0] and sam[0] <= sar[0]) else sar
         elif(len(sal) == 0 and len(sam) != 0 and len(sar) != 0):
@@ -24,12 +23,9 @@ def DnC(ary):
             ta = sal if(sal[0] <= sam[0]) else sam
         tmp.append(ta[0])
         ta.pop(0)
-        if(len(sal) != 0 and len(sam) == 0 and len(sar) == 0):
-            ta = sal
-        elif(len(sam) != 0 and len(sal) == 0 and len(sar) == 0):
-            ta = sam
-        elif(len(sar) != 0 and len(sal) == 0 and len(sam) == 0):
-            ta = sar
+        ta = sal if(len(sal) != 0 and len(sam) == 0 and len(sar) == 0) else \
+                sam if(len(sam) != 0 and len(sal) == 0 and len(sar) == 0) else \
+                sar if(len(sar) != 0 and len(sal) == 0 and len(sam) == 0) else []
         for i in ta:
             tmp.append(i)
             ta.pop(0)
